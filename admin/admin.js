70c56b740920c56f216e01b921fee3c42f58f035
yachty66 <68147541+yachty66@users.noreply.github.com>
2021-11-04 17:02:11 +0100

    update

--- admin/admin.js
+++ admin/admin.js
@@ -46,12 +46,8 @@ function onClickAddStudentsButton(){
 }
 
 function onClickCheckForm(){
-    var test = document.getElementById('Student_Id').value
-    var li = "<li>" + test + "<li>";
-    document.getElementById("list").insertAdjacentHTML('beforeend', li);
-    document.getElementById("Student_Id").value = ""; // clear the value
 
-
+    var Student_Id = document.getElementById('Student_Id').value
     var First_Name = document.getElementById('First_Name').value
     var Last_Name = document.getElementById('Last_Name').value
     var DOB = document.getElementById('DOB').value
@@ -61,24 +57,61 @@ function onClickCheckForm(){
     var option = e.options[e.selectedIndex].text;
     var Email_Id = document.getElementById('Email_Id').value
 
-    if(Student_Id == null || Student_Id == "", First_Name == null || First_Name == "", option == "------", Email_Id == null || Email_Id == ""){
+    if(Student_Id == null || Student_Id == "" || First_Name == null || First_Name == "" || option == "------" || Email_Id == null || Email_Id == ""){
         document.getElementById("error").style.display = "block";
     }else{
-        var list = document.getElementById(list);
-        var listItem = document.createElement("li");
-        listItem.innerText = inputField.value;
-        list.appendChild(listItem);
-        //hier muss ich jetzt die einträge in die studenten liste eintragen und den alten view wieder herstellen
+        document.getElementById("main-for-form").style.display = "none";
+        document.getElementById("main-for-lists").style.display = "flex";
+        document.getElementById("buttons-student").style.display = "flex";
+        document.getElementById("list-order").style.display = "flex";
 
+        var liStudent_Id = "<li>" + "Student_Id: " + Student_Id + "</li>";
+        document.getElementById("list").insertAdjacentHTML('beforeend', liStudent_Id);
+        document.getElementById("Student_Id").value = ""; // clear the value
+
+        var liFirst_Name = "<li>" + "First_Name: " + First_Name + "</li>";
+        document.getElementById("list").insertAdjacentHTML('beforeend', liFirst_Name);
+        document.getElementById("First_Name").value = ""; // clear the value
+
+        var liLast_Name = "<li>" + "Last_Name: " + Last_Name + "</li>";
+        document.getElementById("list").insertAdjacentHTML('beforeend', liLast_Name);
+        document.getElementById("Last_Name").value = ""; // clear the value
+
+        var DOB = "<li>" + "DOB: " + DOB + "</li>";
+        document.getElementById("list").insertAdjacentHTML('beforeend', DOB);
+        document.getElementById("DOB").value = ""; // clear the value
+
+        var liDepartments = "<li>" + "Department: " + option + "</li>";
+        document.getElementById("list").insertAdjacentHTML('beforeend', liDepartments);
+        document.getElementById("Departments").value = ""; // clear the value
+
+        var liEmail_Id = "<li>" + "Email_Id: " + Email_Id + "</li>";
+        document.getElementById("list").insertAdjacentHTML('beforeend', liEmail_Id);
+        document.getElementById("Email_Id").value = ""; // clear the valu
+
+        if (document.getElementById('GenderFemale').checked == false && document.getElementById('GenderMale').checked == false){
+            var liGenderFemale = "<li>" + "Gender: " + "</li>";
+            document.getElementById("list").insertAdjacentHTML('beforeend', liGenderFemale);
+        }
+
+        if (document.getElementById('GenderFemale').checked == true){
+            var liGenderFemale = "<li>" + "Gender: " + "Female" + "</li>";
+            document.getElementById("list").insertAdjacentHTML('beforeend', liGenderFemale);
+            var line = "<hr>";
+            document.getElementById("list").insertAdjacentHTML('beforeend', line);
+            document.getElementById("Gender_Female").value = ""; // eventuell muss ich das noch clearen 
+        }else if(document.getElementById('GenderMale').checked == true){
+            var liGenderMale = "<li>" + "Gender: " + "Male" + "</li>";
+            document.getElementById("list").insertAdjacentHTML('beforeend', liGenderMale);
+            var line = "<hr>";
+            document.getElementById("list").insertAdjacentHTML('beforeend', line);
+            document.getElementById("Gender_Male").value = ""; // eventuell muss ich das noch clearen 
+        }else{
+            var line = "<hr>";
+            document.getElementById("list").insertAdjacentHTML('beforeend', line);
+
+        }
     }
-
-    
-
-    
-}
-
-function test(){
-    document.getElementById("error").style.display = "block";
 }
 
 
